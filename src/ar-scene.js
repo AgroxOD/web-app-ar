0fc6dd9271d03c7ba0d1efb5f1d95dc79e7bde67
AgroxOD <viktor.semenovych@agro-market.ua>
2025-06-06 12:40:43 +0300

    Add MindAR CSS import

--- src/ar-scene.js
+++ src/ar-scene.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { MindARThree } from 'mind-ar/dist/mindar-image-three.prod.js';
+import 'mind-ar/dist/mindar-image-three.prod.css';
 
 // Управление цветом рамки
 function setFrameColor(color) {
