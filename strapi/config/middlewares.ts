ffb2df9074fc9b8c90b0fd22f4cc8cf02783a10d
AgroxOD <viktor.semenovych@agro-market.ua>
2025-06-10 16:13:51 +0000

    Add Strapi

---
+++ strapi/config/middlewares.ts
@@ -0,0 +1,10 @@
+module.exports = [
+  // ...
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["https://web-app-ar.onrender.com", "http://localhost:5173"], // твой фронтенд-URL
+    },
+  },
+];
