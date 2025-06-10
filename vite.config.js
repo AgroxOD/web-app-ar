220ee8c4c3343ea8fd1c2f520ecef1fa2fda86d4
AgroxOD <viktor.semenovych@agro-market.ua>
2025-06-10 23:19:28 +0300

    chore: scope postcss config to frontend

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import postcss from './src/postcss.config.js';
 
 export default defineConfig({
   base: '/web-app-ar/',
   build: { target: 'esnext' },
+  css: { postcss },
 });
