9d8b6868b7529845f7aa5a25215a2c69c15030d3
AgroxOD <viktor.semenovych@agro-market.ua>
2025-06-17 17:34:50 +0300

    fix: update postcss config path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import postcss from './ar-app/src/postcss.config.js';
+import postcss from './postcss.config.js';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ mode }) => {
